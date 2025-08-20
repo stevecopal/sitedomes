04a808edb0ae1a7a85ca1c9756dd78cb3e49c9ec
copalsteve <copalsteve@gmail.com>
2025-08-20 16:24:52 +0100

    modification du style et du dashboard admin ainsi que l'ajout de la fonctionalite de chargement lorsque on clique sur un lien

--- copal/urls.py
+++ copal/urls.py
@@ -19,8 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),  # Pour changer la langue
     path('admin/', admin.site.urls),
     path('', include('domestique.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += i18n_patterns(
+    path('', include('domestique.urls')),
+)
